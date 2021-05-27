19d008636df059cf44449d9b966f13bc14017ef9
Milan <milanvpage@gmail.com>
2021-05-27 08:30:23 -0700

    commit tree 33efa43dcee1c3fdc36c3db35818f8a9ff59ef9f

--- app.rb
+++ app.rb
@@ -20,6 +20,7 @@ class App < Sinatra::Base
 
   get '/set' do
     # set the :foo key of the session hash equal to 'hello' here!
+    session[:foo] = "hello"
     if session[:foo] == 'hello'
       redirect '/fetch'
     else
@@ -32,14 +33,15 @@ class App < Sinatra::Base
   end
 
   get '/second_exercise' do
-    "Your second lesson will be to set your session :id key to a value.\nIn the route: /set_session, write a line of code that sets the session[:id] equal to 1.\nThen, navigate to the '/set_session' path"
+    "Your second lesson will be to set your session :user_id key to a value.\nIn the route: /set_session, write a line of code that sets the session[:user_id] equal to 1.\nThen, navigate to the '/set_session' path"
   end
 
   get '/set_session' do
-    #set session id here
+    session[:id] = 1
+
 
     if session[:id] == 1
-      # "Session ID set. It's currently set to #{session[:id]}."
+
       redirect '/fetch_session_id'
     else
       "Session ID has not been set!"
@@ -51,7 +53,8 @@ class App < Sinatra::Base
   end
 
   get '/logout' do
-    #clear session hash here
+    session = {}
+
     "Session has now been cleared. session content: #{session.inspect}. Continue on to the '/finish' line!"
   end
 
